75191354c8cef8bf4599db7d467740bf8c14f87a
Michèle Wieland <75903906+wielandmichele@users.noreply.github.com>
2024-07-29 15:56:56 +0200

    Update Demographics.py

--- pages/Demographics.py
+++ pages/Demographics.py
@@ -6,7 +6,6 @@ from sqlalchemy import create_engine, text
 import pymysql
 import sqlalchemy
 import os
-import paramiko
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 from fabric import Connection
